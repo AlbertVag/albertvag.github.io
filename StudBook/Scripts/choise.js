adf8dedb348154aa123c9cffbb9c31ecced26734
AlbertVag <49408871+AlbertVag@users.noreply.github.com>
2019-05-07 15:19:51 +0300

    2

---
+++ StudBook/Scripts/choise.js
@@ -0,0 +1,9 @@
+function onchangeUnSelect() {
+    var modelUn = document.getElementById('faculty');
+    modelUn.style.display = "";
+}
+
+function onchangeFucSelect() {
+    var modelFuc = document.getElementById('group');
+    modelFuc.style.display = "";
+}
